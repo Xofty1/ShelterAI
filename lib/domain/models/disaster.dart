0be34228165f4c17debc6d0a7e1efc50f1fc4a66
TaxMax <arz.taxedo@gmail.com>
2025-03-27 18:13:26 +0300

    add game settings cubit, start main game bloc, add domain models

---
+++ lib/domain/models/disaster.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'disaster.freezed.dart';
+
+@freezed
+abstract class Disaster with _$Disaster{
+  const factory Disaster({
+    required String name,
+    required String disasterDescription,
+    required String disasterShortDescription,
+    required String shelterName,
+    required String location,
+    required String description,
+    required int capacity,
+    required List<String> rooms,
+    required List<String> resources,
+  }) = _Disaster;
+}
