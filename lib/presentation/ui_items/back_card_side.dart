662860a0e8c5912b495ab3caa8287f1b3a626e62
KolaYAndr <312263@edu.itmo.ru>
2025-04-03 14:57:47 +0300

    changed player card detail

--- lib/presentation/ui_items/back_card_side.dart
+++ lib/presentation/ui_items/back_card_side.dart
@@ -3,20 +3,57 @@ import 'package:flutter/material.dart';
 import 'asset_image_item.dart';
 
 class PlayerCardBack extends StatelessWidget {
-  const PlayerCardBack({super.key});
+
+  final int currentIndex;
+
+  const PlayerCardBack({
+    super.key,
+    required this.currentIndex
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
-        AssetImageItem(
+      Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: const Color(0xFFD9D9D9),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Text(
+                currentIndex.toString(),
+              style: const TextStyle(
+                color: Color(0xFF9E4A42),
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+              ),
+        ),
+      ),
+        const SizedBox(height: 110),
+        const AssetImageItem(
           imagePath: "assets/images/door.svg",
           width: 200,
           height: 200,
         ),
-        SizedBox(height: 20),
-        Text(
+        const SizedBox(height: 24),
+        const Text(
           "Нажмите, чтобы увидеть карту игрока",
           textAlign: TextAlign.center,
           style: TextStyle(
