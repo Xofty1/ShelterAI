80288d1dbc651d34c71e65841e2e231e5dedaf27
onetaper-gleb <tigertankp@gmail.com>
2025-04-03 16:01:29 +0300

    small twicks

--- lib/domain/services/gpt_repository.dart
+++ lib/domain/services/gpt_repository.dart
@@ -4,8 +4,6 @@ import 'package:shelter_ai/domain/models/game_settings.dart';
 import '../models/player.dart';
 
 abstract interface class GptRepository {
-  // Future<Disaster> getDisaster(GameSettings settings);
-  // Future<List<Player>> getPlayers(GameSettings settings);
 
   Future<String> getFinale(GameSettings settings, Disaster disaster,
       List<Player> alivePlayers, List<Player> kickedPlayer);
@@ -13,6 +11,4 @@ abstract interface class GptRepository {
   Future<Disaster> createGame(GameSettings settings);
 
   Future<List<Player>> createPlayers(GameSettings settings, Disaster disaster);
-
-  // Future<Disaster> createStory(GameSettings settings);
 }
